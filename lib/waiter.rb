46ae80c0b7b6433528d3a1f1fad7bc9aeceb5bf4
James Mobus <mobusj@live.com>
2019-09-22 18:34:12 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,11 +1,13 @@
 class Waiter
-  attr_reader :name, :years_of_experience
+  attr_reader :name, :years_of_experience, :meals, :tips
   
   @@all = []
   
   def initialize(name, years_of_experience)
     @name = name
     @years_of_experience = years_of_experience
+    @meals = []
+    @tips = []
     
     @@all << self 
   end
@@ -13,4 +15,8 @@ class Waiter
   def self.all
     @@all
   end
+  
+  def new_meal(customer, total, tip)
+    @tips.push(tip)
+    @meals.push(new Meal(self, customer, total, tip))
 end
